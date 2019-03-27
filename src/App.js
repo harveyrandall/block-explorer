ccdcf57f22b40ccb4d3937411e30a4db7f5918c0
Harvey Randall <harvey_randall@me.com>
2019-03-27 21:42:34 +0000

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/header';
+import Home from './components/home';
+import Address from './components/address';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-      </div>
+        <Router>
+            <div className="App">
+                <Header />
+
+                <Route exact path="/" component={Home} />
+                <Route path="/wallet" component={Address} />
+            </div>
+        </Router>
     );
   }
 }
